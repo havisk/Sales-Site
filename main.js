004b1f108447c5971ed1baa3ff07449d0d8bf997
Kool Havis <havisk@gmail.com>
2015-10-02 19:33:40 -0400

    Finished first 2 questions

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 (function () {
 
+
   // Create an array of just the prices
   var prices = items.map( function (item) {
     return item.price;
@@ -26,13 +27,54 @@
 
 
   // Make it show up on the page
-
+  
     // Find our answer element
   var answer1 = document.querySelector('#answer1');
     // Create a node from our above answer ready for the DOM
   var textNode = document.createTextNode(str);
     // Append newly created node to our answer element
   answer1.appendChild(textNode);
+  
+
+  
+    // filter items based on price
+    var cost = items.filter(function(item){
+    
+      return item.price > 14 &&
+             item.price < 18;
+    });
+      
+      var y = cost.map(function(x){
+        return x.title;
+      })
+
+      console.log(y);
+
+      var item = y.forEach(function(name){
 
 
+      var ul = document.createElement('ul');
+      // Find our answer element
+
+        var answer2 = document.querySelector('#answer2');
+        // Create a node from our above answer ready for the DOM
+        var textNode = document.createTextNode(name);
+        //create list element
+        var li = document.createElement('li');
+        // Append newly created node to our answer element
+        
+        li.appendChild(textNode);
+        ul.appendChild(li);
+
+        console.log(name);
+        answer2.appendChild(ul);
+      });
+
+      //
+        
+
+
+
+
+  
 }());
